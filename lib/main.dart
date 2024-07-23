a1b408ccae338a03d38e18566a1bbf5f2f6fbde6
Stiven Grisales <carlosstivengrisales@gmail.com>
2024-07-23 07:43:40 -0500

    enviroment

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:app_cinemapedia/config/route/route.dart';
 import 'package:app_cinemapedia/config/theme/app_theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
