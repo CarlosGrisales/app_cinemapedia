b20333fa7aeb23236a63f25d13a669075750e846
Stiven Grisales <carlosstivengrisales@gmail.com>
2024-07-23 10:09:49 -0500

    movies provider

---
+++ lib/presentation/screens/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,10 @@
+import 'package:app_cinemapedia/infraestructure/datasources/moviedb_datasource.dart';
+import 'package:app_cinemapedia/infraestructure/repositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Este repositorio es inmutable
+final movieRepositoryProvider = Provider((ref) {
+
+  return MovieRepositoryImpl(MoviedbDatasource());
+
+});
